27d1b1daaeeba5596dcd6288bbf7e21a43a6221d
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-21 19:18:09 -0400

    v0.0.13

--- Casks/toy.rb
+++ Casks/toy.rb
@@ -1,8 +1,8 @@
 cask "toy" do
-  version "0.0.12"
+  version "0.0.13"
   sha256 :no_check
 
-  url "https://github.com/eu-ge-ne/homebrew-tap/releases/download/v0.0.12/toy.zip"
+  url "https://github.com/eu-ge-ne/homebrew-tap/releases/download/v0.0.13/toy.zip"
   name "toy"
   homepage "https://github.com/eu-ge-ne/homebrew-tap"
 
