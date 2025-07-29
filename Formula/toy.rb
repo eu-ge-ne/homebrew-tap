43b4e61bcc221db907122bf5075cc2512e35353c
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-29 19:30:05 -0400

    1

--- Formula/toy.rb
+++ Formula/toy.rb
@@ -1,4 +1,4 @@
-class Toy < Formula
+﻿class Toy < Formula
   desc "Minimalistic TUI text editor"
   homepage "https://github.com/eu-ge-ne/toy"
   url "https://github.com/eu-ge-ne/toy/archive/refs/tags/v0.0.19.tar.gz"
@@ -17,6 +17,6 @@ class Toy < Formula
   end
 
   test do
-    assert_equal "toy 0.0.19", shell_output("#{bin}/toy --version")
+    assert_equal "toy 0.0.19\n", shell_output("#{bin}/toy --version")
   end
 end
