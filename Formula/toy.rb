25e6ed164727e2d1a0f16af47e34efc220723f15
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-07-29 19:38:10 -0400

    v0.0.19 (#3)

--- Formula/toy.rb
+++ Formula/toy.rb
@@ -1,14 +1,12 @@
 class Toy < Formula
   desc "Minimalistic TUI text editor"
   homepage "https://github.com/eu-ge-ne/toy"
-  url "https://github.com/eu-ge-ne/toy/archive/refs/tags/v0.0.18.tar.gz"
-  sha256 "cf654952e97306050c5e45e5434e6833a0a86c8cbe573bff15660393472041b1"
+  url "https://github.com/eu-ge-ne/toy/archive/refs/tags/v0.0.19.tar.gz"
+  sha256 "d425b4a9e23cdd3e65fb0394026776e605cda5b4b05d670e6b39e006e2c8b7d5"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/eu-ge-ne/toy/releases/download/v0.0.18"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9727afce83e7f9876cc67ba7eea5c92fa733083c8577aa4405c3825c89a04277"
+    root_url "https://github.com/eu-ge-ne/toy/releases/download/v0.0.19"
   end
 
   depends_on "deno"
@@ -19,6 +17,6 @@ class Toy < Formula
   end
 
   test do
-    assert_equal "toy 0.0.18\n", shell_output("#{bin}/toy --version")
+    assert_equal "toy 0.0.19\n", shell_output("#{bin}/toy --version")
   end
 end
