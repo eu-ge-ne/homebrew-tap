5a8068d48bce1c296621ccad31fd95900793df6a
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2025-07-30 04:25:56 -0400

    v0.0.20 (#7)

--- Formula/toy.rb
+++ Formula/toy.rb
@@ -1,14 +1,13 @@
 class Toy < Formula
   desc "Minimalistic TUI text editor"
   homepage "https://github.com/eu-ge-ne/toy"
-  url "https://github.com/eu-ge-ne/toy/archive/refs/tags/v0.0.19.tar.gz"
-  sha256 "d425b4a9e23cdd3e65fb0394026776e605cda5b4b05d670e6b39e006e2c8b7d5"
+  url "https://github.com/eu-ge-ne/toy/archive/refs/tags/v0.0.20.tar.gz"
+  sha256 "4dbf2d26f64729d569e8634a22d2120504836702a7c8d99c0c8a412a204f7b72"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/eu-ge-ne/toy/releases/download/v0.0.19"
-    rebuild 3
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "46e4b2b73469b6ac22d6723858765086ea1eebe68b77b4c73ea1d917181f4e65"
+    root_url "https://github.com/eu-ge-ne/toy/releases/download/v0.0.20"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0db000bdca1c455f99039eca3bcb90817dbda0f86791181ec21aa38a61c14967"
   end
 
   depends_on "deno"
@@ -19,6 +18,6 @@ class Toy < Formula
   end
 
   test do
-    assert_equal "toy 0.0.19\n", shell_output("#{bin}/toy --version")
+    assert_equal "toy 0.0.20\n", shell_output("#{bin}/toy --version")
   end
 end
