deda234a1222d313e3c90b222e32f53db96e13ed
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-07-30 04:22:07 -0400

    1

--- Formula/toy.rb
+++ Formula/toy.rb
@@ -2,7 +2,7 @@ class Toy < Formula
   desc "Minimalistic TUI text editor"
   homepage "https://github.com/eu-ge-ne/toy"
   url "https://github.com/eu-ge-ne/toy/archive/refs/tags/v0.0.20.tar.gz"
-  sha256 "3e2e1f8bf02ac4ae084b2ae1a6f4c55b0126804e3638260f5658511b6b432ec1"
+  sha256 "4dbf2d26f64729d569e8634a22d2120504836702a7c8d99c0c8a412a204f7b72"
   license "MIT"
 
   bottle do
