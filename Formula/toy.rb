16fdc1e51182e667fe44e22f474cfd9ef6d55d1b
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-08-05 13:42:18 -0400

    toy 0.0.24

--- Formula/toy.rb
+++ Formula/toy.rb
@@ -1,8 +1,8 @@
 class Toy < Formula
   desc "Minimalistic TUI text editor"
   homepage "https://github.com/eu-ge-ne/toy"
-  url "https://github.com/eu-ge-ne/toy/archive/refs/tags/v0.0.23.tar.gz"
-  sha256 "2581347bff15722d08261c14b24a971ff829a04be691ff8774c906823b1a690c"
+  url "https://github.com/eu-ge-ne/toy/archive/refs/tags/v0.0.24.tar.gz"
+  sha256 "e1477627363d2500883e334615b7f98405ef9022373696d943a45160c04389a7"
   license "MIT"
 
   depends_on "deno" => [:build]
