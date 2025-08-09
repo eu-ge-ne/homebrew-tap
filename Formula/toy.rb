c767d43bc9510b8e5a55f9509ff3a2aee1a27196
Eugene Pobochny <eugene.pobochny@gmail.com>
2025-08-08 22:12:19 -0400

    test

--- Formula/toy.rb
+++ Formula/toy.rb
@@ -19,6 +19,6 @@ class Toy < Formula
   end
 
   test do
-    assert_equal "toy 0.0.24\n", shell_output("#{bin}/toy --version")
+    assert_equal "toy 0.0.25\n", shell_output("#{bin}/toy --version")
   end
 end
