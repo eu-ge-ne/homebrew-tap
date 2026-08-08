2bef5801b5a23f8c4accb9786988e6e3a45d076e
Eugene Pobochny <eu-ge-ne@users.noreply.github.com>
2026-08-08 19:29:22 -0400

    toy 0.0.31

--- Formula/toy.rb
+++ Formula/toy.rb
@@ -1,10 +1,9 @@
 class Toy < Formula
   desc "Minimalistic TUI text editor"
   homepage "https://github.com/eu-ge-ne/toy"
-  url "https://github.com/eu-ge-ne/toy/archive/refs/tags/v0.0.30.tar.gz"
-  sha256 "84c506eaa424634c9cc0381edc7ac1f4e3a045487e4a3cc8ef08814f258fe1f3"
+  url "https://github.com/eu-ge-ne/toy/archive/refs/tags/v0.0.31.tar.gz"
+  sha256 "56c30e9f7f82695f95eb21e6c33b8ed84c6579279c6e7b5aa34569998f68998a"
   license "MIT"
-  revision 2
 
   bottle do
     root_url "https://github.com/eu-ge-ne/homebrew-tap/releases/download/toy-0.0.30_2"
